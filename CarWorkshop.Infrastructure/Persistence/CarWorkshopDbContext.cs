e13d5b8cd8b9d42d342959c6e47c489304e28069
KTokarski2 <krzysztof.tokarski2@icloud.com>
2023-08-13 19:08:47 +0200

    create db context

--- CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -5,5 +5,15 @@ namespace CarWorkshop.Infrastructure.Persistence;
 public class CarWorkshopDbContext : DbContext
 {
     public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
-    
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=CarWorkshopDb;Encrypt=false;TrustServerCertificate=True;Integrated Security=False;User Id=SA;Password=H0m0$apien$123$$$;MultipleActiveResultSets=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+            .OwnsOne(c => c.ContactDetails);
+    }
 }
