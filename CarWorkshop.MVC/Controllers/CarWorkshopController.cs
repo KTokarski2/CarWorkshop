615e297d29c52b1bb7375fb2f54434317cc443ac
KTokarski2 <krzysztof.tokarski2@icloud.com>
2023-08-14 00:25:08 +0200

    create view for create carworkshop action

--- CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -12,6 +12,12 @@ public class CarWorkshopController : Controller
     {
         _carWorkshopService = carWorkshopService;
     }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View();
+    }
     
     [HttpPost]
     public async Task<IActionResult> Create(Domain.Entities.CarWorkshop carWorkshop)
